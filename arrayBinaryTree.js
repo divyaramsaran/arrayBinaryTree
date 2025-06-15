e49360fecc99a84139ea68dc95703a68515f832b
Ram Saran <bagydivyaramsaran@gmail.com>
2025-06-15 11:03:40 +0000

    converted array of string integers to array numbers

--- arrayBinaryTree.js
+++ arrayBinaryTree.js
@@ -5,25 +5,7 @@ const stringToNum = (str) => {
 function ArrayChallenge(strArr) {
   // code goes here
   const integerArray = strArr.map(stringToNum);
-  console.log(integerArray);
-
-  return integerArray.reduce(
-    ([isBinaryTree, uniqueList], integers) => {
-      const hid = uniqueList.reduce((init, list) => {
-        if (list.includes(integers[0]) || list.includes(integers[1])) {
-          return true;
-        }
-
-        uniqueList.push(integers);
-        return false;
-      });
-
-      return [hid, uniqueList];
-    },
-    [true, []]
-  );
-
-  return strArr;
+  return integerArray;
 }
 
 const strArr = ["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"];
