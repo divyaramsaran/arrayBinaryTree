aaf1981a7428b23c0f7a425e1d3a701cc5a75750
Ram Saran <bagydivyaramsaran@gmail.com>
2025-06-16 12:35:15 +0000

    added isBinary function and moved while to it

--- arrayBinaryTree.js
+++ arrayBinaryTree.js
@@ -8,26 +8,9 @@ const stringToNum = (str) => {
   );
 };
 
-const entries = (integerArray, target) => {
-  return integerArray.filter(([child, root]) => target[0] === root);
-};
-
-const isBinaryTree = (integerArray, rootNode) => {
-  if (rootNode.length === 0) {
-    return true;
-  }
-
-  const nodeEntries = integerArray.filter(
-    ([child, root]) => rootNode[0] === root
-  );
-
-  if (nodeEntries.length > 2 || rootNode.length > 2) {
-    return false;
-  }
-
+const isBinaryTree = (integerArray, nodeEntries) => {
   while (nodeEntries.length !== 0) {
     const element = nodeEntries[0];
-
     const entry = element !== undefined ? entries(integerArray, element) : [];
 
     if (entry.length > 2) {
@@ -40,7 +23,28 @@ const isBinaryTree = (integerArray, rootNode) => {
 
     nodeEntries.shift();
   }
+
   return true;
+};
+
+const entries = (integerArray, target) => {
+  return integerArray.filter(([child, root]) => target[0] === root);
+};
+
+const validateBinarytree = (integerArray, rootNode) => {
+  if (rootNode.length === 0) {
+    return true;
+  }
+
+  const nodeEntries = integerArray.filter(
+    ([child, root]) => rootNode[0] === root
+  );
+
+  if (nodeEntries.length > 2 || rootNode.length > 2) {
+    return false;
+  }
+
+  return isBinaryTree(integerArray, nodeEntries);
 };
 
 function ArrayChallenge(strArr) {
@@ -50,7 +54,7 @@ function ArrayChallenge(strArr) {
     .filter(([child, parent]) => !childNodes.includes(parent))
     .flat();
 
-  return isBinaryTree(integerArray, rootNode);
+  return validateBinarytree(integerArray, rootNode);
 }
 
 const strArr = ["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)", "(3,5)"];
